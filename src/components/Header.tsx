80a4c0fa72415e0987d4f553aa8a8a467ff68381
spammenotinoz <spammenotinoz@gmail.com>
2023-09-26 08:07:13 +1000

    Update Header.tsx
    Cleanup header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import Logo from '@/components/icons/Logo';
 import ThemeToggle from '@/components/ThemeToggle';
-import {GITHUB_ORG_URL} from '@/utils/constants';
 
 const Header = () => {
     return (
@@ -13,16 +12,6 @@ const Header = () => {
                 <span className="gpt-title">MIDJOURNEY</span>
                 <span className="gpt-subtitle">WEB</span>
             </div>
-            <p className="mt-1 opacity-60 ">
-                Powered by
-                <a
-                    className="b-buy-link ml-1 decoration-wavy underline-offset-8"
-                    href={ GITHUB_ORG_URL } target="_blank"
-                    rel="noopener noreferrer"
-                >
-                     ConnectAI-E
-                </a>
-            </p>
         </header>
     );
 };
