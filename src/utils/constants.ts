ca906377bfbe609a647f89c03d39b9cac825ace7
spammenotinoz <spammenotinoz@gmail.com>
2023-09-26 07:36:28 +1000

    Update constants.ts
    https://artificin.com/prompt-builder/

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,6 +3,6 @@ export const SITE_DESC = 'Supercharged Experience For MidJourney On Web UI'
 
 export const MAX_TOKENS = 500
 export const RATE_LIMIT_COUNT = 5
-export const OPS_URL = 'https://moonvy.com/apps/ops/'
+export const OPS_URL = 'https://artificin.com/prompt-builder/'
 export const GITHUB_ORG_URL = 'https://github.com/ConnectAI-E'
 
