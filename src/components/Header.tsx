5544e262f06209281da812bdb51afdc2868631a5
spammenotinoz <spammenotinoz@gmail.com>
2023-10-13 23:21:27 +1100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import MJW_STATS from '@/utils/constants';
+import {MJW_STATS} from '@/utils/constants';
 import Logo from '@/components/icons/Logo';
 import ThemeToggle from '@/components/ThemeToggle';
 
