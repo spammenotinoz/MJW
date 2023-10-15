eb082faf8d266a149a190585b6987b22c75aa919
spammenotinoz <spammenotinoz@gmail.com>
2023-10-15 23:48:51 +1100

    Update SendArea.tsx

--- src/components/SendArea.tsx
+++ src/components/SendArea.tsx
@@ -1,77 +1,114 @@
-import {FunctionComponent} from 'react';
+import { FunctionComponent, useEffect, useRef, useState, useMemo } from 'react';
+import { forwardRef, useImperativeHandle } from 'react';
 import ClearIcon from './icons/Clear';
 import UploadImage from '@/components/UploadImage';
-import {useAtom} from 'jotai';
-import {uploadImagesUrlAtom} from '@/hooks/useUplodImage';
+import { useAtom } from 'jotai';
+import { uploadImagesUrlAtom } from '@/hooks/useUplodImage';
 
 interface SendAreaProps {
-    handleKeydown: (e: KeyboardEvent) => void;
-    handleButtonClick: () => void;
-    handleStopClick: () => void;
-    clear: () => void;
-    inputRefItem: any;
-    ifLoading: boolean;
-    ifDisabled: boolean;
+  handleKeydown: (e: KeyboardEvent) => void;
+  handleButtonClick: () => void;
+  handleStopClick: () => void;
+  clear: () => void;
+  inputRefItem: any;
+  ifLoading: boolean;
+  ifDisabled: boolean;
 }
 
-const SendArea = forwardRef(({
-                                                        ifDisabled,
-                                                        handleKeydown,
-                                                        handleButtonClick,
-                                                        handleStopClick,
-                                                        clear,
-                                                        inputRefItem,
-                                                        ifLoading,
-                                                    }:SendAreaProps,ref) => {
-
+const SendArea: FunctionComponent<SendAreaProps> = forwardRef(
+  (
+    {
+      ifDisabled,
+      handleKeydown,
+      handleButtonClick,
+      handleStopClick,
+      clear,
+      inputRefItem,
+      ifLoading,
+    }: SendAreaProps,
+    ref
+  ) => {
     const textareaRef = useRef(null);
     const [imageSrcs, setImageSrcs] = useState<string[]>([]);
-    const [imageUploadSrc,setImageUpload] = useAtom(uploadImagesUrlAtom)
+    const [imageUploadSrc, setImageUpload] = useAtom(uploadImagesUrlAtom);
+    const [stats, setStats] = useState<string>(''); // Initialize stats as an empty string
+
     useEffect(() => {
-        setImageUpload(imageSrcs)
-        // if(imageSrcs.length===0) return
-        console.log(imageSrcs);
-    },[imageSrcs])
+      setImageUpload(imageSrcs);
+      // if(imageSrcs.length===0) return
+      console.log(imageSrcs);
+    }, [imageSrcs]);
 
     const handlePaste = (event: any) => {
-        const items = (event.clipboardData || event.originalEvent.clipboardData).items;
-        const imageFiles = [] as any;
+      const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+      const imageFiles = [] as any;
 
-        for (const item of items) {
-            if (item.type.indexOf('image') !== -1) {
-                const file = item.getAsFile();
-                const imageUrl = URL.createObjectURL(file);
-                imageFiles.push(imageUrl);
-            }
+      for (const item of items) {
+        if (item.type.indexOf('image') !== -1) {
+          const file = item.getAsFile();
+          const imageUrl = URL.createObjectURL(file);
+          imageFiles.push(imageUrl);
         }
-        setImageSrcs([...imageSrcs, ...imageFiles]);
+      }
+      setImageSrcs([...imageSrcs, ...imageFiles]);
     };
 
-    function emptyImagesSrc(){
-        setImageSrcs([])
+    function emptyImagesSrc() {
+      setImageSrcs([]);
     }
 
     useImperativeHandle(ref, () => ({
-        emptyImagesSrc
+      emptyImagesSrc,
     }));
 
     const handleDeleteByIndex = (index: number) => {
-        const newImageSrcs = [...imageSrcs];
-        newImageSrcs.splice(index, 1);
-        setImageSrcs(newImageSrcs);
-    }
+      const newImageSrcs = [...imageSrcs];
+      newImageSrcs.splice(index, 1);
+      setImageSrcs(newImageSrcs);
+    };
+
     const description = useMemo(() => {
-        const imageLength = imageSrcs.length;
-        return imageLength > 0 ? imageLength>1? `Click Send to Blend ${imageLength} uploaded images`:
-            `Click Send to describe 1 uploaded image` : "Enter image description here...";
-    }, [imageSrcs])
+      const imageLength = imageSrcs.length;
+      return imageLength > 0
+        ? imageLength > 1
+          ? `Click Send to Blend ${imageLength} uploaded images`
+          : `Click Send to describe 1 uploaded image`
+        : 'Enter image description here...';
+    }, [imageSrcs]);
 
     const inDescriptionOrBlend = useMemo(() => {
-        const imageLength = imageSrcs.length;
-        return imageLength > 0 
-    }, [imageSrcs])
+      const imageLength = imageSrcs.length;
+      return imageLength > 0;
+    }, [imageSrcs]);
+
+    // Function to fetch and update the stats every 5 minutes
+    const fetchAndUpdateStats = async () => {
+      try {
+        // Make an HTTP request to fetch the stats data
+        const response = await fetch('/stats.txt');
+        if (response.ok) {
+          const statsData = await response.text();
+          // Update the stats
+          setStats(statsData);
+        } else {
+          console.error('Failed to fetch stats data');
+        }
+      } catch (error) {
+        console.error('Error fetching stats data:', error);
+      }
+    };
+
+    // Update the stats initially and every 5 minutes (300,000 milliseconds)
+    useEffect(() => {
+      fetchAndUpdateStats(); // Fetch initially
+      const intervalId = setInterval(fetchAndUpdateStats, 300000);
+
+      // Cleanup the interval when the component unmounts
+      return () => clearInterval(intervalId);
+    }, []);
+
     return (
-      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-40px md:max-w-[90%] md:w-[560px] sm:w-[90%] lg:w-[50%] backdrop-blur-md pt-1 px-4 pb-4 z-100 text-[16px] rounded-md">  
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-40px md:max-w-[90%] md:w-[560px] sm:sm:w-[90%] backdrop-blur-md pt-1 px-4 pb-4 z-100 text-[16px] rounded-md">
         {ifLoading ? (
           <div className="gen-cb-wrapper">
             <span>Thinking...</span>
@@ -93,7 +130,7 @@ const SendArea = forwardRef(({
             )}
             <div
               className={
-                ifDisabled ? "op-50 gen-text-wrapper" : "gen-text-wrapper"
+                ifDisabled ? 'op-50 gen-text-wrapper' : 'gen-text-wrapper'
               }
             >
               <textarea
@@ -106,7 +143,7 @@ const SendArea = forwardRef(({
                 disabled={ifDisabled || inDescriptionOrBlend}
                 autoFocus
                 onInput={() => {
-                  inputRefItem.current.style.height = "auto";
+                  inputRefItem.current.style.height = 'auto';
                   inputRefItem.current.style.height = `${inputRefItem.current.scrollHeight}px`;
                 }}
                 className="gen-textarea"
@@ -128,10 +165,13 @@ const SendArea = forwardRef(({
                 <ClearIcon />
               </button>
             </div>
+            {/* Center the stats text on a new row */}
+            <div className="text-center mt-2">{stats}</div>
           </div>
         )}
       </div>
     );
-})
+  }
+);
 
 export default SendArea;
