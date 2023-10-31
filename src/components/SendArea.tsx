fcddac8c3f9b3f15ad39997acf4a35764c540d42
spammenotinoz <spammenotinoz@gmail.com>
2023-10-31 13:20:54 +1100

    Update SendArea.tsx

--- src/components/SendArea.tsx
+++ src/components/SendArea.tsx
@@ -108,7 +108,7 @@ const SendArea: FunctionComponent<SendAreaProps> = forwardRef(
     }, []);
 
     return (
-      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-40px md:max-w-[90%] md:w-[560px] sm:sm:w-[90%] backdrop-blur-md pt-1 px-4 pb-4 z-100 text-[16px] rounded-md">
+      <div className="fixed left-1/2 transform -translate-x-1/2 bottom-40px md:max-w-[90%] md:w-[560px] sm:w-[98%] backdrop-blur-md pt-1 px-4 pb-4 z-100 text-[16px] rounded-md">
         {ifLoading ? (
           <div className="gen-cb-wrapper">
             <span>Thinking...</span>
