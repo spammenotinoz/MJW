b7369823c97ff6ca6d489e47acfb40948a4189ab
spammenotinoz <spammenotinoz@gmail.com>
2024-02-24 16:26:25 +1100

    Update SignIn.tsx

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 //import React, { useState } from 'react';
+import {Header} from '@/components/Header';
 import { supabase } from '../utils/supabaseClient';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
@@ -21,11 +22,14 @@ export function SignIn() {
     };
 
     return (
+        <div>
+        <Header/>
         <form onSubmit={handleSignIn}>
             <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <button type="submit">Sign In</button>
         </form>
+        </div>
     );
 }
 
