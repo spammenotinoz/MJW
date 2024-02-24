4832d9060b2848952a2d56eb4476783822188080
spammenotinoz <spammenotinoz@gmail.com>
2024-02-24 21:18:24 +1100

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,7 +1,4 @@
-// src/utils/supabaseClient.js
-import { createBrowserClient } from "@supabase/ssr"
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
