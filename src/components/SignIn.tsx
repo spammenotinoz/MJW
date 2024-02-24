dfc71f532202e5f46a986fba6341a9fb05cb5b83
spammenotinoz <spammenotinoz@gmail.com>
2024-02-24 16:23:18 +1100

    Update SignIn.tsx

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,5 @@
-//import * as React from 'react';
-import React, { useState } from 'react';
+import * as React from 'react';
+//import React, { useState } from 'react';
 import { supabase } from '../utils/supabaseClient';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
