588b4784ba3f7d93d262a8b37f03aca033c3decb
spammenotinoz <spammenotinoz@gmail.com>
2024-02-24 21:20:43 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
             <Router>
                 <main>
                     {session ? (
-                        <SignIn />
+                        <ChatPage />
                     ) : (
                         <Routes>
                             <Route path="" element={<ChatPage />} />
