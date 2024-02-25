ee9a0cb6543c0b3588d733679795508cb412f5e1
spammenotinoz <spammenotinoz@gmail.com>
2024-02-25 19:19:41 +1100

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -24,9 +24,7 @@ export function SignIn() {
         <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">           
         <form onSubmit={handleSignIn}>          
             <input type="email" className="mb-3 rounded-md border bg-inherit px-4 py-2" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="you@example.com" required />
-            required
-        />
-            
+                        
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
               Login
