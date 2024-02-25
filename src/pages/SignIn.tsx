a5671b488d3a5b0229787f4ce867f11d2f3a7fdb
spammenotinoz <spammenotinoz@gmail.com>
2024-02-25 20:27:10 +1100

    Update SignIn.tsx

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import {Header} from '@/components/Header';
 import { supabase } from '../utils/supabaseClient';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
-//import { Brand } from "@/components/brand";
-import { SubmitButton } from "@/components/submit-button";
+import { Brand } from "@/components/brand";
+//import { SubmitButton } from "@/components/submit-button";
 
 export function SignIn() {
     const [email, setEmail] = useState('');
@@ -25,6 +25,7 @@ export function SignIn() {
     return (
         <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">        
         <form
+            
             className="animate-in text-foreground flex w-full flex-1 flex-col justify-center gap-2"
             onSubmit={handleSignIn}
         >
@@ -36,9 +37,9 @@ export function SignIn() {
                 className="mb-6 rounded-md border bg-inherit px-4 py-2"
                 type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="********" required 
             />
-            <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
-              Login
-            </SubmitButton>
+//            <SubmitButton className="mb-2 rounded-md bg-blue-700 px-4 py-2 text-white">
+//              Login
+//            </SubmitButton>
         </form>
         </div>
     );
